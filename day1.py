ee97f6238b24b23044b6260e9aa9d77e963a6ea1
praneeth <praneethk1124@gmail.com>
2025-09-02 23:35:19 +0530

    new code

--- day1.py
+++ day1.py
@@ -1,1 +1,36 @@
 print("Hello, World!")
+
+# this is a comment 
+# print is used to display output on the screen
+
+# input
+name = input("enter your name : ")
+print(name)
+
+'''
+a,b = input("enter two numbers")
+print(a)
+print(b)
+'''
+
+
+
+x,y = input("enter two numbers : ").split()
+print(x)
+print(y)
+
+
+#indentation
+
+a = ("this is  praneeth")
+b = ("from malla reddy collage")
+print(a)
+print(b)
+
+# indentation error
+a = ("this is  praneeth") 
+b = ("from malla reddy collage")
+print(a)
+print(b)
+
+
