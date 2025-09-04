110b547e2c15ac8e6f0b34f02ba1e59b386d66c4
praneeth <praneethk1124@gmail.com>
2025-09-04 21:32:11 +0530

    Types

---
+++ day3.py
@@ -0,0 +1,27 @@
+# DataTypes inPython
+# int
+a = 17 #it is an integer
+# float 
+b = 25.26 # it is a float  number with "." init is float 
+'''complex
+# A complex number has a real and an imaginary part, and is denoted as 
+# a + bj, where a is the real part and b is the imaginary part.
+We can access the real and imaginary parts of the complex numbers using .real and .imag methods respectively.
+'''
+z = 9 + 8j
+z2 = 5 - 3j
+x = z + z2
+print(x)
+# stry
+k = "hey this is about data types"
+# this line is string str
+# boolean
+x = 10
+y = 5
+
+print(f"Is x equal to y? {x == y}")
+print(f"Is x greater than y? {x > y}")
+print(f"Is x not equal to y? {x != y}")
+boolean_value = bool(1)
+print(boolean_value)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-boolean_value = bool(1)
-print(boolean_value)
+x = 10
+y = 5
+
+print(f"Is x equal to y? {x == y}")
+print(f"Is x greater than y? {x > y}")
+print(f"Is x not equal to y? {x != y}")
