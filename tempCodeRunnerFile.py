9b02d57310993f703061d07ccc2eb1445a16a5d5
praneeth <praneethk1124@gmail.com>
2025-09-09 23:25:48 +0530

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,17 @@
-x = "python"
-print(x[0:4])  
-print(x[:3])
-print(x[::-5])
+#Arthematic operators
+a = 26
+b = 17
+# Addition
+print( a + b)  
+# Subtraction
+print( a - b) 
+# Multiplication
+print(a * b)  
+# Division
+print(a / b) 
+# Floor Division
+print( a // b)  
+# Modulus
+print(a % b) 
+# Exponentiation
+print( a ** b)
