0bd1b845f67e5260d002d7a3abec46d8a39b00a8
praneeth <praneethk1124@gmail.com>
2025-09-10 17:08:35 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,41 @@
+#Arthematic operators
+a = 26
+b = 17
+# Addition
+print( a + b)  
+# Subtraction
+print( a - b) 
+# Multiplication
+print(a * b)  
+# Division
+print(a / b) 
+# Floor Division
+print( a // b)  
+# Modulus
+print(a % b) 
+# Exponentiation
+print( a ** b)
+# Comparison operators
+x = 10 
+y = 20
+# Equal to  
+print(x == y) 
+# Not equal to
+print(x != y) 
+# Greater than
+print(x > y)
+# Less than
+print(x < y)    
+# Greater than or equal to
+print(x >= y)
+# Less than or equal to
+print(x <= y)
+# Logical operators
+p = True
+q = False
+# Logical AND
+print(p and q)
+# Logical OR
+print(p or q)
+# Logical NOT
+print(not p)
