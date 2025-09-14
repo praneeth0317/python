d5501cced4e7767cf0f545de6cf2367a7a6dbfba
praneeth <praneethk1124@gmail.com>
2025-09-14 11:43:40 +0530

    set

--- sets.py
+++ sets.py
@@ -6,3 +6,18 @@ print(set(t))
 
 x = set(["praneeth","kumar","reddy"])
 print(x)
+
+myset = {"apple", "banana", "cherry"}
+print(myset)
+myset = set(["apple", "banana", "cherry"])
+print(myset)
+mixed_set = {"hello", 42, 3.14, (1, 2)}
+print(mixed_set)
+myset.add("orange")
+print(myset)
+myset.remove("banana")  
+myset.discard("banana") 
+"apple" in myset 
+print(myset)
+len(myset)
+
