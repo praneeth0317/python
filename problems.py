9d6eab2c8c62f1ac12f4878889280fc70022d258
praneeth <praneethk1124@gmail.com>
2025-09-19 21:15:49 +0530

    problems

--- problems.py
+++ problems.py
@@ -1,10 +1,6 @@
 '''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
-Example 1:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Constraints:
 2 <= nums.length <= 104
 -109 <= nums[i] <= 109
@@ -24,18 +20,6 @@ class Solution:
     
 
 """Given an integer x, return true if x is a palindrome, and false otherwise.
-Example 1:
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
-Example 2:
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 Constraints:
 -231 <= x <= 231 - 1
 Follow up: Could you solve it without converting the integer to a string?"""
@@ -70,18 +54,6 @@ I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
-Example 1:
-Input: s = "III"
-Output: 3
-Explanation: III = 3.
-Example 2
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
-Example 3:
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 Constraints:
 1 <= s.length <= 15
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
