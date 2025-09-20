62b47f473b43944af9e2d5ec16408fd225cc2a9a
praneeth <praneethk1124@gmail.com>
2025-09-20 18:45:49 +0530

    problems

--- problems.py
+++ problems.py
@@ -72,3 +72,20 @@ class Solution:
                 total += value
             prev = value
         return total
+'''Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string .
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.'''
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
