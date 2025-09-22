678379126213365f1fb3e39979fd033d2e6958b8
praneeth <praneethk1124@gmail.com>
2025-09-22 21:37:17 +0530

    problems

--- problems.py
+++ problems.py
@@ -109,3 +109,33 @@ class Solution:
                 if not stack or mapping[char] != stack.pop():
                     return False
         return not stack
+
+'''You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        curr.next = list1 if list1 else list2
+        return dummy.next
+
