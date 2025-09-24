9be21dcd716b87dd2b232ebc4a1bf9e67b958514
praneeth <praneethk1124@gmail.com>
2025-09-24 16:03:41 +0530

    updated

--- problems.py
+++ problems.py
@@ -192,3 +192,11 @@ Constraints:
 0 <= nums[i] <= 50
 0 <= val <= 100'''
 
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
