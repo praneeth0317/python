e7529457b3da49331df1763c8629992279dcba5e
praneeth <praneethk1124@gmail.com>
2025-09-25 13:50:14 +0530

    updated

--- problems.py
+++ problems.py
@@ -200,3 +200,8 @@ class Solution:
                 nums[k] = nums[i]
                 k += 1
         return k
+
+'''Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Constraints:
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.'''
