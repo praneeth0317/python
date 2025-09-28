d9e362b3401f27de93d63b97ec01d5577cfd40f6
praneeth <praneethk1124@gmail.com>
2025-09-28 17:22:22 +0530

    loops

--- loops.py
+++ loops.py
@@ -3,3 +3,10 @@ for i in range(5):
     print("Iteration:", i)
 print("Loop finished")
  
+x = 3
+for i in range (x):
+    print("Current value:", i)
+
+x = 4
+for i in range(1, 10, 2):
+    print("Current value:", i)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,3 @@
-for i in range(5):
-    print("Iteration:", i)
-print("Loop finished")
+x = 4
+for i in range(1, 10, 2):
+    print("Current value:", i)
