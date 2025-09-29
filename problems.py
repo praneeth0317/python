409ea09c5ba52c300636a2355f5665c2f218320e
praneeth <praneethk1124@gmail.com>
2025-09-29 22:53:53 +0530

    updated

--- problems.py
+++ problems.py
@@ -217,3 +217,16 @@ Constraints:
 -104 <= nums[i] <= 104
 nums contains distinct values sorted in ascending order.
 -104 <= target <= 104'''
+
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left  # Insert position if not found
