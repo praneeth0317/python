bcf7f6153f9d22d018af807b7ecbce5215356fdf
praneeth <praneethk1124@gmail.com>
2025-09-29 23:45:18 +0530

    updated

--- loops.py
+++ loops.py
@@ -7,10 +7,24 @@ x = 3
 for i in range (x):
     print(":", i)
 
-
 for i in range(1, 10, 2):
     print("Current value:", i)
 print("Loop with step finished")
 
 for i in range(0, 15, 1):
     print("current value:", i)
+
+# --- IGNORE ---
+#loops
+# for i in range(5):
+#     print("Iteration:", i)
+# print("Loop finished")
+#
+# x = 4
+# for i in range(1, 10, 2):
+#     print("Current value:", i)
+# --- IGNORE ---
+# --- IGNORE ---
+#loops
+# for i in range(5):
+#     print("Iteration:", i)
