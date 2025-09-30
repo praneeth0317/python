faff9ed159f4edf404ab9d84d384e0fd292f3cd2
praneeth <praneethk1124@gmail.com>
2025-09-30 23:49:51 +0530

    updated

--- problems.py
+++ problems.py
@@ -230,3 +230,9 @@ class Solution:
             else:
                 right = mid - 1
         return left  # Insert position if not found
+'''Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+Constraints:
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.'''
