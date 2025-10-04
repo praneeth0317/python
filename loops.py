7260e0ff02db72cfd45e6eae6fe72603523e5803
praneeth <praneethk1124@gmail.com>
2025-10-04 22:55:34 +0530

    updated

--- loops.py
+++ loops.py
@@ -93,3 +93,8 @@ for i in range(1, 21):
         continue
     print(i)
 print("Loop with continue finished")
+for i in range(1, 21):
+    if i % 3 == 0 or i % 5 == 0:
+        continue
+    print(i)
+print("Loop with continue finished")
