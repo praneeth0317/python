177cacca94959d8d00aabf7b737c415fb94aac61
praneeth <praneethk1124@gmail.com>
2025-10-05 00:38:18 +0530

    updated

--- loops.py
+++ loops.py
@@ -107,12 +107,3 @@ for color in colors:
 else:
     print("Target not found in the list.")
 
-with open("yourfile.txt", "rb") as file:
-    file_bytes_iterator = iter(file.read())
-    
-    for b in file_bytes_iterator:
-        if b == 0:  # null byte check
-            break
-        # process byte b
-    else:
-        raise ValueError("String is unterminated")
