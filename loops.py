5d219aae533538cb32105de3dc8e57fb3b5a019a
praneeth <praneethk1124@gmail.com>
2025-10-05 00:37:51 +0530

    updated

--- loops.py
+++ loops.py
@@ -107,11 +107,12 @@ for color in colors:
 else:
     print("Target not found in the list.")
 
-result = ""
-for b in file_bytes_iterator:
-    if b == '\0':
-        break
-    result += b
-else:
-    raise ValueError("String is unterminated")
-print(result)
+with open("yourfile.txt", "rb") as file:
+    file_bytes_iterator = iter(file.read())
+    
+    for b in file_bytes_iterator:
+        if b == 0:  # null byte check
+            break
+        # process byte b
+    else:
+        raise ValueError("String is unterminated")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,9 @@
-for i in range(10):
-    if i == 5:
-        break
-    print(i)
+with open("yourfile.txt", "rb") as file:
+    file_bytes_iterator = iter(file.read())
+    
+    for b in file_bytes_iterator:
+        if b == 0:  # null byte check
+            break
+        # process byte b
+    else:
+        raise ValueError("String is unterminated")
