d0463c32558de9b910b6d4c64270d93043891963
praneeth <praneethk1124@gmail.com>
2025-10-09 23:02:19 +0530

    updated

--- loops.py
+++ loops.py
@@ -122,3 +122,11 @@ while count < 5:
 else:
     print("Loop ended naturally")
 
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for num in numbers:
+    if num > 500:
+        break
+    if num > 150:
+        continue
+    if num % 5 == 0:
+        print(num)
