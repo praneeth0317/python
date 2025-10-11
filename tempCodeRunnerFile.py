74cbbc60428a91739c79c7a9889c1b1993a28e9f
praneeth <praneethk1124@gmail.com>
2025-10-11 23:13:59 +0530

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,9 @@
-age = int(input())
-if age >= 18:
-    print("eligible to vote")
-else:
-    print("not eligible to vote")
+def check_voting_eligibility(age):
+    if age >= 18:
+        print("eligible to vote")
+    else:
+        print("not eligible to vote")
+
+for i in range(3):  # Checks eligibility for 3 people
+    age = int(input("Enter age: "))
+    check_voting_eligibility(age)
