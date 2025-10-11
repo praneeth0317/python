6a92ceb4784a6e9dab1c6cc212a45697ea5bb5cb
praneeth <praneethk1124@gmail.com>
2025-10-11 23:07:32 +0530

    updated

--- problems.py
+++ problems.py
@@ -279,3 +279,8 @@ class Solution:
             result.append(str(total % 2))
             carry = total // 2
         return ''.join(reversed(result))
+'''Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+Constraints:
+0 <= x <= 231 - 1'''
