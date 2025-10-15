855ec69f5d13d901348fcd92b9dce3a99c39dfbe
praneeth <praneethk1124@gmail.com>
2025-10-15 20:55:20 +0530

    updated

--- problems.py
+++ problems.py
@@ -284,3 +284,18 @@ You must not use any built-in exponent function or operator.
 For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 Constraints:
 0 <= x <= 231 - 1'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+                ans = mid
+            else:
+                right = mid - 1
+        return ans  
