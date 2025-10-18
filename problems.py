545a78975c7c8fe4df6497713b63c6a817c6184d
praneeth <praneethk1124@gmail.com>
2025-10-18 19:51:43 +0530

    updated

--- problems.py
+++ problems.py
@@ -345,7 +345,6 @@ class Solution:
     def merge(self, nums1, m, nums2, n):
         i, j, k = m - 1, n - 1, m + n - 1
 
-        # Merge from the end
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -355,7 +354,6 @@ class Solution:
                 j -= 1
             k -= 1
 
-        # Copy remaining nums2 elements (if any)
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
