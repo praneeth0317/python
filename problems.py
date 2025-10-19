904e13adb0767df9094d396959a176a7160f7638
praneeth <praneethk1124@gmail.com>
2025-10-19 20:24:10 +0530

    updated

--- problems.py
+++ problems.py
@@ -367,4 +367,3 @@ class Solution:
     def inorderTraversal(self, root):
         if not root:
             return []
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
