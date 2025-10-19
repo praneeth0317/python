3a2cdd50e007a8f4ef85acde6e2e31f43360cb01
praneeth <praneethk1124@gmail.com>
2025-10-19 17:34:54 +0530

    updated

--- problems.py
+++ problems.py
@@ -358,3 +358,8 @@ class Solution:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
+'''Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+Follow up: Recursive solution is trivial, could you do it iteratively?'''
