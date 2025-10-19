39ff8600e9f8258bf939d930a5ed48619bb5f66d
praneeth <praneethk1124@gmail.com>
2025-10-19 17:35:34 +0530

    updated

--- problems.py
+++ problems.py
@@ -363,3 +363,8 @@ Constraints:
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 Follow up: Recursive solution is trivial, could you do it iteratively?'''
+class Solution:
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
