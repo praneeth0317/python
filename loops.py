52630007f9d971791ededee769367096c0f9b69c
praneeth <praneethk1124@gmail.com>
2025-10-22 22:54:01 +0530

    updated

--- loops.py
+++ loops.py
@@ -204,3 +204,9 @@ for i in range(1, N + 1):
     for j in range(i): 
         print(i, end=" ")
     print()
+
+n = 5
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
