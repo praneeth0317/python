bad05893759d7a791ce97dbf4ac0097fae459db3
praneeth <praneethk1124@gmail.com>
2025-10-23 20:05:06 +0530

    updated

--- problems.py
+++ problems.py
@@ -379,7 +379,6 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
