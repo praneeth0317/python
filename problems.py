7e5dd3f905b7ce283c8da086267a9dc5508dc45f
praneeth <praneethk1124@gmail.com>
2025-10-23 20:04:13 +0530

    updated

--- problems.py
+++ problems.py
@@ -374,3 +374,16 @@ Constraints:
 The number of nodes in both trees is in the range [0, 100].
 -104 <= Node.val <= 104
 '''
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
