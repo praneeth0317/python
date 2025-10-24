4fe2f9bfb291c2ae22265ba52cd3ec1cd0e8ad08
praneeth <praneethk1124@gmail.com>
2025-10-24 22:28:41 +0530

    updated

--- problems.py
+++ problems.py
@@ -386,3 +386,8 @@ class Solution:
         if not p or not q or p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+'''Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+Follow up: Could you solve it both recursively and iteratively?'''
