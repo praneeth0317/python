bac5eb96120e236b4b60695357230f443820267d
praneeth <praneethk1124@gmail.com>
2025-10-25 18:00:00 +0530

    Work from Oct 25 (backdated)

--- loops.py
+++ loops.py
@@ -245,4 +245,9 @@ for i in range(1, N + 1):
     for j in range():
         print((5-i)*"*",end="")
     print()
-
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+'''Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+Follow up: Could you solve it both recursively and iteratively?'''
