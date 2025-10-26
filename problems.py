fa8fefa698a14af2871128e1c21942d1f219f782
praneeth <praneethk1124@gmail.com>
2025-10-26 19:17:03 +0530

    updated

--- problems.py
+++ problems.py
@@ -391,3 +391,16 @@ Constraints:
 The number of nodes in the tree is in the range [1, 1000].
 -100 <= Node.val <= 100
 Follow up: Could you solve it both recursively and iteratively?'''
+
+'''Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+Each letter in pattern maps to exactly one unique word in s.
+Each unique word in s maps to exactly one letter in pattern.
+No two letters map to the same word, and no two words map to the same letter.
+Constraints:
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.'''
