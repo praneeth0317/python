19fa56def1676e399675d6e04c756cc57217a4a8
praneeth <praneethk1124@gmail.com>
2025-10-27 19:22:03 +0530

    updated

--- problems.py
+++ problems.py
@@ -428,3 +428,25 @@ Constraints:
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        return prev
+def reverseListRecursive(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        p = self.reverseListRecursive(head.next)
+        head.next.next = head
+        head.next = None
+        return
