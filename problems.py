52bca8017db55fa1bb93fe73b1cc62c89ec0f62c
praneeth <praneethk1124@gmail.com>
2025-10-28 23:47:16 +0530

    updated

--- problems.py
+++ problems.py
@@ -450,3 +450,8 @@ def reverseListRecursive(self, head: ListNode) -> ListNode:
         head.next.next = head
         head.next = None
         return
+'''Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Constraints
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+Follow up: Could you solve it both recursively and iteratively?'''
