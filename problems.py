11bb622aac7063e7b9c9448f16c819337e577e82
praneeth <praneethk1124@gmail.com>
2025-10-28 23:48:38 +0530

    updated

--- problems.py
+++ problems.py
@@ -455,3 +455,35 @@ Constraints
 The number of nodes in the tree is in the range [1, 1000].
 -100 <= Node.val <= 100
 Follow up: Could you solve it both recursively and iteratively?'''
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val) and \
+                   isMirror(t1.left, t2.right) and \
+                   isMirror(t1.right, t2.left)
+        return isMirror(root, root)
+
+def isSymmetricIterative(self, root: TreeNode) -> bool:
+        from collections import deque
+        queue = deque([(root, root)])
+        while queue:
+            t1, t2 = queue.popleft()
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+        return True
