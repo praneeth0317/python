beb5c358b15d8d6befa03b2e591d5fa26255773f
praneeth <praneethk1124@gmail.com>
2025-10-30 21:02:42 +0530

    updated

--- loops.py
+++ loops.py
@@ -302,3 +302,12 @@ for i in range(n):
                 if j<=i:
                     print(j+1,end="")
     print()
+
+N = 5
+for i in range(1,5):
+    for j in range(1,5): 
+        print("",end=" ")
+    for j in range():
+        print((5-i)*"*",end="")
+    print()
+#   1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,8 @@
-
-n = 5
-for i in range(n):
-    for j in range(i,n):
-        print(" ",end="")
-    for j in range(i+1):
-                if j<=i:
-                    print(j+1,end="")
+N = 5
+for i in range(1,5):
+    for j in range(1,5): 
+        print("",end=" ")
+    for j in range():
+        print((5-i)*"*",end="")
     print()
+#   1
