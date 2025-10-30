975ed55c9e65cca968dcf5f2560a5cfacfdee13d
praneeth <praneethk1124@gmail.com>
2025-10-30 19:00:05 +0530

    updated

--- problems.py
+++ problems.py
@@ -493,3 +493,21 @@ Constraints:
 The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.'''
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            carry, val = divmod(val1 + val2 + carry, 10)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+                
+        return dummy.next
