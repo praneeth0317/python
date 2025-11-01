b1597af12e8b9faf826c76ef2a1b9587df1d6e0a
praneeth <praneethk1124@gmail.com>
2025-11-01 21:07:13 +0530

    updated

--- problems.py
+++ problems.py
@@ -528,3 +528,12 @@ class Solution:
             max_length = max(max_length, i - start + 1)
 
         return max_length
+'''Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+Constraints:
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106'''
