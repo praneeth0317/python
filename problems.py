7f7edf8e88349f291e7d22b007b7ee9498e89364
praneeth <praneethk1124@gmail.com>
2025-11-01 21:09:03 +0530

    updated

--- problems.py
+++ problems.py
@@ -537,3 +537,29 @@ nums2.length == n
 0 <= n <= 1000
 1 <= m + n <= 2000
 -106 <= nums1[i], nums2[i] <= 106'''
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Ensure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and nums2[j-1] > nums1[i]:
+                imin = i + 1  # i is too small
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                imax = i - 1  # i is too big
+            else:
+                # i is perfect
+                if i == 0: max_of_left = nums2[j-1]
+                elif j == 0: max_of_left = nums1[i-1]
+                else: max_of_left = max(nums1[i-1], nums2[j-1])
+                if (m + n) % 2 == 1:
+                    return float(max_of_left)
+                if i == m: min_of_right = nums2[j]
+                elif j == n: min_of_right = nums1[i]
+                else: min_of_right = min(nums1[i], nums2[j])
+                return (max_of_left + min_of_right) / 2.0
