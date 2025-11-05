f56095eaeced220ae36c5da924a1feda3fb60062
praneeth <praneethk1124@gmail.com>
2025-11-05 22:17:37 +0530

    updated

--- problems.py
+++ problems.py
@@ -639,3 +639,13 @@ class Solution:
         if result < INT_MIN or result > INT_MAX:
             return 0
         return result
+'''Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+The algorithm for myAtoi(string s) is as follows:
+Whitespace: Ignore any leading whitespace (" ").
+Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+Return the integer as the final result.
+Constraints:
+0 <= s.length <= 200
+s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.'''
