f31acc4911bb65e166345e3f6f66683908478cb7
praneeth <praneethk1124@gmail.com>
2025-11-06 22:25:12 +0530

    updated

--- problems.py
+++ problems.py
@@ -674,3 +674,13 @@ class Solution:
             i += 1
         
         return sign * result
+'''Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+Constraints:
+1 <= s.length <= 20
+1 <= p.length <= 20
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.'''
