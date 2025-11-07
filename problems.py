5722e7266754f4c8a019f9f0c1f87ec3bb8126ec
praneeth <praneethk1124@gmail.com>
2025-11-07 20:00:09 +0530

    updated

--- problems.py
+++ problems.py
@@ -711,20 +711,3 @@ Constraints:
 n == height.length
 2 <= n <= 105
 0 <= height[i] <= 104'''
-class Solution:
-    def maxArea(self, height: list[int]) -> int:
-        left, right = 0, len(height) - 1
-        max_water = 0
-        
-        while left < right:
-            width = right - left
-            h = min(height[left], height[right])
-            area = width * h
-            max_water = max(max_water, area)
-            
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-        
-        return max_water
