0325b810e0b66c5c8bc8e413ef0933382dcf4173
praneeth <praneethk1124@gmail.com>
2025-11-07 19:58:22 +0530

    updated

--- problems.py
+++ problems.py
@@ -703,3 +703,11 @@ class Solution:
                     if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
                         dp[i][j] = dp[i][j] or dp[i - 1][j]
         return dp[n][m]
+'''You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+Constraints:
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104'''
