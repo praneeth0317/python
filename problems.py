8f6d7e3950f972916d10330233500c170c190be8
praneeth <praneethk1124@gmail.com>
2025-11-09 18:34:58 +0530

    updated

--- problems.py
+++ problems.py
@@ -762,3 +762,8 @@ class Solution:
             roman.append(syms[i] * count)
             num %= val[i]
         return "".join(roman)
+'''Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+Constraints:
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105'''
