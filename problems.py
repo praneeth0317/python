95d151a8eaa1d8387b4dcb19dba3ccba8316be89
praneeth <praneethk1124@gmail.com>
2025-11-09 18:37:43 +0530

    updated

--- problems.py
+++ problems.py
@@ -767,3 +767,30 @@ Notice that the solution set must not contain duplicate triplets.
 Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105'''
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        n = len(nums)
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return res
