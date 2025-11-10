f390562cc496e4e36d5c7f783d5fbfc5bb788d79
praneeth <praneethk1124@gmail.com>
2025-11-10 19:46:23 +0530

    updated

--- problems.py
+++ problems.py
@@ -819,3 +819,22 @@ Constraints:
 3 <= nums.length <= 500
 -1000 <= nums[i] <= 1000
 -104 <= target <= 104'''
+class Solution:
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        n = len(nums)
+        closest_sum = float('inf')
+
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                if abs(target - curr_sum) < abs(target - closest_sum):
+                    closest_sum = curr_sum
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+                else:
+                    return curr_sum
+        return closest_sum
