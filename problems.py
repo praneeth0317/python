9d12d14501be9823ac314505db28445de126f03b
praneeth <praneethk1124@gmail.com>
2025-11-11 21:10:03 +0530

    updated

--- problems.py
+++ problems.py
@@ -825,29 +825,3 @@ A mapping of digits to letters (just like on the telephone buttons) is given bel
 Constraints:
 1 <= digits.length <= 4
 digits[i] is a digit in the range ['2', '9'].'''
-class Solution:
-    def letterCombinations(self, digits):
-        if not digits:
-            return []
-        phone_map = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
-        }
-
-        res = []
-
-        def backtrack(index, path):
-            if index == len(digits):
-                res.append("".join(path))
-                return
-            for letter in phone_map[digits[index]]:
-                backtrack(index + 1, path + [letter])
-
-        backtrack(0, [])
-        return res
