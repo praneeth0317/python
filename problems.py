3a999f50e38fbaa85bbd4ca2c642a3307995dfbe
praneeth <praneethk1124@gmail.com>
2025-11-14 22:34:32 +0530

    updated

--- problems.py
+++ problems.py
@@ -895,3 +895,24 @@ Constraints:
 The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        curr.next = list1 if list1 else list2
+        return dummy.next
