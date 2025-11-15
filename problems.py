839fa03d9b5fe983db2e8b5388d420128ed8cdc4
praneeth <praneethk1124@gmail.com>
2025-11-15 21:56:01 +0530

    updated

--- problems.py
+++ problems.py
@@ -916,3 +916,20 @@ class Solution:
 
         curr.next = list1 if list1 else list2
         return dummy.next
+'''Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8'''
