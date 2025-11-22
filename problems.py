e6008fc2ac5d18d3bd84024ff4672af3ee526edb
praneeth <praneethk1124@gmail.com>
2025-11-22 19:47:25 +0530

    updated

--- problems.py
+++ problems.py
@@ -1157,3 +1157,8 @@ class Solution:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
+            
+'''Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+Constraints:
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.'''
