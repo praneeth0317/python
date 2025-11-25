292d2d1ba5111d2256662a4bcc003096cd12a17b
praneeth <praneethk1124@gmail.com>
2025-11-25 16:34:27 +0530

    updated

--- problems.py
+++ problems.py
@@ -1214,14 +1214,6 @@ class Solution:
 If target is not found in the array, return [-1, -1].
 
 You must write an algorithm with O(log n) runtime complexity.
-
- 
-
-Example 1:
-
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-Example 2:
 Constraints:
 0 <= nums.length <= 105
 -109 <= nums[i] <= 109
