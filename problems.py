8a689324076b88a2c258598aaa6af9e200737c20
praneeth <praneethk1124@gmail.com>
2025-11-25 16:33:15 +0530

    updated

--- problems.py
+++ problems.py
@@ -1237,3 +1237,25 @@ Constraints:
 -109 <= nums[i] <= 109
 nums is a non-decreasing array.
 -109 <= target <= 109'''
+class Solution:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        def findBound(isLeft):
+            left, right = 0, len(nums) - 1
+            bound = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    bound = mid
+                    if isLeft:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return bound
+
+        start = findBound(True)
+        end = findBound(False)
+        return [start, end]
