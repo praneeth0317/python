90607e63f70bbe9dce8252892104d8f3770430ef
praneeth <praneethk1124@gmail.com>
2025-11-26 18:00:00 +0530

    updated

--- problems.py
+++ problems.py
@@ -1210,9 +1210,7 @@ class Solution:
                     
         return -1
 '''Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
-
 If target is not found in the array, return [-1, -1].
-
 You must write an algorithm with O(log n) runtime complexity.
 Constraints:
 0 <= nums.length <= 105
