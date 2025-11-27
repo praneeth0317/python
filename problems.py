a16edb9cef2a97acedadf9621d0ea0ab16427af3
praneeth <praneethk1124@gmail.com>
2025-11-27 20:46:50 +0530

    updated

--- problems.py
+++ problems.py
@@ -1239,25 +1239,4 @@ class Solution:
         start = findBound(True)
         end = findBound(False)
         return [start, end]
-    class Solution:
-    def searchRange(self, nums: list[int], target: int) -> list[int]:
-        def findBound(isLeft):
-            left, right = 0, len(nums) - 1
-            bound = -1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] == target:
-                    bound = mid
-                    if isLeft:
-                        right = mid - 1
-                    else:
-                        left = mid + 1
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return bound
-
-        start = findBound(True)
-        end = findBound(False)
-        return [start, end]
+    
