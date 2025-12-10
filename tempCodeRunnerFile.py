712ea8992ed7fc30c45f4732fe69e4d58f0f28a5
praneeth <praneethk1124@gmail.com>
2025-12-10 23:30:21 +0530

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,9 @@
 
-w1 = input("Enter first word: ").lower()
-w2 = input("Enter second word: ").lower()
+n = int(input("How many terms? "))
 
-if len(w1) != len(w2):
-    print("Not an Anagram")
-else:
-    used = [False] * len(w2)
+a = 0
+b = 1
 
-    is_anagram = True
-
-    for i in range(len(w1)):
-        found = False
-        for j in range(len(w2)):
-            if (w1[i] == w2[j]) and (not used[j]):
-                used[j] = True
-                found = True
-                break
-        if not found:
-            is_anagram = False
-            break
-
-    if is_anagram:
-        print("Anagram")
-    else:
-        print("Not an Anagram")
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
