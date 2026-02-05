af46be1c9d3603287c9d41e9a851e07d8c6499c4
praneeth <praneethk1124@gmail.com>
2026-02-05 18:00:00 +0530

    updated

--- problems.py
+++ problems.py
@@ -1427,6 +1427,5 @@ class Solution:
     
 '''Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
 You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space. 
-Constraints:
 1 <= nums.length <= 105
 -231 <= nums[i] <= 231 - 1'''
