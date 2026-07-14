6f2d51b37f8f9318d8f951ddfdcf4bead12f732a
praneeth <praneethk1124@gmail.com>
2026-07-14 18:00:00 +0530

    updated

--- problems.py
+++ problems.py
@@ -1453,3 +1453,14 @@ You must implement an algorithm that runs in O(n) time and uses O(1) auxilia
 Constraints:
 1 <= nums.length <= 105
 -231 <= nums[i] <= 231 - 1'''
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1
